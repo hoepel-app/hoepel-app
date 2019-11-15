91b8a6c47311671b8645ae89539a41d321a3de73
Thomas Toye <thomas@toye.io>
2019-11-15 21:35:59 +0100

    Ad test for schema

---
+++ packages/firestore-rules/src/schema-to-firestore/create-firestore-rules-from-schema.test.ts
@@ -0,0 +1,48 @@
+import { buildSchema } from "graphql";
+import { createFirestoreRulesFromSchema } from "./create-firestore-rules-from-schema";
+
+const testSchema = buildSchema(`
+enum COLLECTION_TYPE {
+    INDEXED_BY_TENANT
+    DOC_PER_TENANT
+    DOC_PER_USER
+}
+
+directive @firestore(
+    name: String!
+    type: COLLECTION_TYPE!
+    readPermission: String
+    writePermission: String
+    allowDelete: Boolean
+    allowUpdate: Boolean
+    allowPublicRead: Boolean
+) on OBJECT
+
+type AgeGroup @firestore(name:"age-groups", type:DOC_PER_TENANT, readPermission:"age-groups:read", writePermission:"age-groups:write") {
+    id: ID!
+    name: String
+}
+
+type Child @firestore(name:"children", type: INDEXED_BY_TENANT, readPermission:"child:read", writePermission:"child:write", allowUpdate: true, allowDelete: true) {
+    id: ID!
+    name: String
+}
+
+type Consumable @firestore(name:"consumables", type:DOC_PER_TENANT, readPermission:"consumables:read", writePermission:"consumables:write") {
+    id: ID!
+}
+
+type Consumption @firestore(name:"consumptions", type:INDEXED_BY_TENANT, readPermission:"consumption:read", writePermission:"consumption:write", allowUpdate: true, allowDelete: true) {
+    id: ID!
+}
+
+type User @firestore(name:"users", type:DOC_PER_USER) {
+    id: ID!
+}
+`)
+
+describe('createFirestoreRulesFromSchema', () => {
+    it('create Firestore rules from schema', () => {
+        expect(createFirestoreRulesFromSchema(testSchema)).toMatchSnapshot()
+    })
+})
