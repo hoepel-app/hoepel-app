efc4652a8a440e248d3a23ebaa66e0c7b6951e3c
Thomas Toye <thomas@toye.io>
2019-11-15 21:27:56 +0100

    Typo

--- packages/schema/src/schema.ts
+++ packages/schema/src/schema.ts
@@ -3,5 +3,3 @@ import { readFileSync } from "fs"
 import { join } from "path"
 
 export const schema: GraphQLSchema = buildSchema(readFileSync(join(__dirname, '../schema.graphql')).toString())
-
-schema.
