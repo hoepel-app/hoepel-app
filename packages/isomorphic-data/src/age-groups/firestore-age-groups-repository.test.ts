098c3ff313879c616efc3c824215753ac2e24ea3
Thomas Toye <thomas@toye.io>
2020-04-19 11:45:21 +0200

    Fix tests

--- packages/isomorphic-data/src/age-groups/firestore-age-groups-repository.test.ts
+++ packages/isomorphic-data/src/age-groups/firestore-age-groups-repository.test.ts
@@ -1,25 +1,25 @@
-// import * as firebase from '@firebase/testing'
-// import { FirestoreAgeGroupsRepository } from './firestore-age-groups-repository'
-// import { injectTestingAdaptor } from 'typesaurus/testing'
-// import { AgeGroups } from '@hoepel.app/isomorphic-domain'
+import * as firebase from '@firebase/testing'
+import { FirestoreAgeGroupsRepository } from './firestore-age-groups-repository'
+import { injectTestingAdaptor } from 'typesaurus/testing'
+import { AgeGroups } from '@hoepel.app/isomorphic-domain'
 
-// describe('FirestoreAgeGroupsRepository', () => {
-//   let app: ReturnType<typeof firebase.initializeAdminApp> | undefined
+describe('FirestoreAgeGroupsRepository', () => {
+  let app: ReturnType<typeof firebase.initializeAdminApp> | undefined
 
-//   beforeEach(() => {
-//     app = firebase.initializeAdminApp({ projectId: 'project-id' })
-//     injectTestingAdaptor(app)
-//   })
+  beforeEach(() => {
+    app = firebase.initializeAdminApp({ projectId: 'project-id' })
+    injectTestingAdaptor(app)
+  })
 
-//   afterEach(async () => {
-//     await app?.delete()
-//   })
+  afterEach(async () => {
+    await app?.delete()
+  })
 
-//   it('returns default empty age groups when nothing was saved', async () => {
-//     const repo = new FirestoreAgeGroupsRepository()
+  it('returns default empty age groups when nothing was saved', async () => {
+    const repo = new FirestoreAgeGroupsRepository()
 
-//     const result = await repo.findForTenant('my-tenant-name')
+    const result = await repo.findForTenant('my-tenant-name')
 
-//     expect(result).toEqual(AgeGroups.createEmpty())
-//   })
-// })
+    expect(result).toEqual(AgeGroups.createEmpty())
+  })
+})
