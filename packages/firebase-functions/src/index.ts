44cfd28b858394737c62666a1f59d0931ef44b80
Thomas Toye <thomas@toye.io>
2020-04-19 12:08:07 +0200

    Remove backupfire

--- packages/firebase-functions/src/index.ts
+++ packages/firebase-functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-import backupfireAgent from '@backupfire/firebase'
 
 // Init firebase app
 admin.initializeApp(functions.config().firebase)
@@ -10,9 +9,5 @@ db.settings({ timestampsInSnapshots: true })
 export * from '@hoepel.app/firestore-events'
 export * from './user-events/'
 export { onCloudBuildPubsub } from '@hoepel.app/cloud-build-notifier'
-
-export const backupfire = backupfireAgent({
-  region: 'europe-west3',
-})
 
 export * from './api'
