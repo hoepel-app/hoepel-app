6a7d24db8a915c835d59bdd19c0c500c2ba7d3ee
Thomas Toye <thomas@toye.io>
2020-06-20 16:06:03 +0200

    Add backupfire

--- packages/firebase-functions/src/index.ts
+++ packages/firebase-functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import backupfireAgent from '@backupfire/firebase'
 
 // Init firebase app
 admin.initializeApp(functions.config().firebase)
@@ -9,5 +10,9 @@ db.settings({ timestampsInSnapshots: true })
 export * from '@hoepel.app/firestore-events'
 export * from './user-events/'
 export { onCloudBuildPubsub } from '@hoepel.app/cloud-build-notifier'
+
+export const backupfire = backupfireAgent({
+  region: 'europe-west3',
+})
 
 export * from './api'
